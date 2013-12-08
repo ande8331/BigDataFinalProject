e94f0dd84e19dcaf4897834456b4a81cb3bf026c
Ross - Cloudera VM <ande8331@stthomas.edu>
2013-12-07 22:24:33 -0500

    Tracked down issue when trying to add in double header data to the output.  Determined its not appropriate, but also identified bug in how TextComparatorInverted works (doesn't work correctly with strings of different lengths).

--- src/TextComparatorInverted.java
+++ src/TextComparatorInverted.java
@@ -12,6 +12,10 @@ public class TextComparatorInverted extends WritableComparator {
 	
 	@Override
 	public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+		
+		// This happens to work right now, but if the strings being compared were of different length, it wouldn't work...
+		
+		
 		return Text.Comparator.compareBytes(b1, s1, l1, b2, s2, l2) * -1;
     }
 	
